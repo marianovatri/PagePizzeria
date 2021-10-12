a4496a394421670c46d4916345e4b2a2b265938b
marianovatri <marianovatri@gmail.com>
2021-10-12 11:34:22 -0300

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './componentes/info/info.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { DetalleEmpleadoComponent } from './componentes/detalle-empleado/detalle-empleado.component';
+import { ProyectosComponent } from './componentes/empleados/proyectos/proyectos.component';
+import { CurriculumComponent } from './componentes/empleados/curriculum/curriculum.component';
+import { ExperienciaComponent } from './componentes/empleados/experiencia/experiencia.component';
+import { HorariosComponent } from './componentes/horarios/horarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    AboutComponent,
+    ContactComponent,
+    DetalleEmpleadoComponent,
+    ProyectosComponent,
+    CurriculumComponent,
+    ExperienciaComponent,
+    HorariosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/empleados/proyectos/proyectos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-proyectos',
+  templateUrl: './proyectos.component.html',
+  styleUrls: ['./proyectos.component.css']
+})
+export class ProyectosComponent implements OnInit {
+
+  empleadoId: number;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+
+    this.empleadoId= 0
+   }
+
+  ngOnInit(): void {
+   
+
+  }
+
+}
