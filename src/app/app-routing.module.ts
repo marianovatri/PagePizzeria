e5ce0bb0fa25878a1fb0d6399d2a6fdc90f9b586
marianovatri <marianovatri@gmail.com>
2021-10-14 10:04:10 -0300

    cleaning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,33 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './componentes/about/about.component';
 import { ContactComponent } from './componentes/contact/contact.component';
-import { DetalleEmpleadoComponent } from './componentes/detalle-empleado/detalle-empleado.component';
-import { CurriculumComponent } from './componentes/empleados/curriculum/curriculum.component';
-import { ExperienciaComponent } from './componentes/empleados/experiencia/experiencia.component';
-import { ProyectosComponent } from './componentes/empleados/proyectos/proyectos.component';
 import { HorariosComponent } from './componentes/horarios/horarios.component';
-import { InfoComponent } from './componentes/info/info.component';
-import { RandomGuard } from './random.guard';
+
 
 const routes: Routes = [
-  // Esto es la ruta raiz ( es como si escribieramos solo localhost4200)
+
   { path: '', pathMatch: 'full', redirectTo: 'home'},
-  //
-{ path: 'informacion', component: InfoComponent, canActivate: [RandomGuard]},
+
+
 { path: 'about', redirectTo: 'home'},
 { path: 'home', component: AboutComponent},
 { path: 'contacto', component: ContactComponent},
 { path: 'horarios', component: HorariosComponent},
-// todo lo que se vea en una ruta despues de ":" significa que se puede modificar.
-{ path: 'empleados/:empleadoId', component: DetalleEmpleadoComponent, children:[
-  {path: 'proyectos', component: ProyectosComponent},
-  {path: 'experiencia', component: ExperienciaComponent},
-  {path: 'curriculum', component: CurriculumComponent}
-
-]},
-  // esto es la ruta comodin (en caso de que escriban un url dentro del localhost inexistente)
-  { path: '**', redirectTo: 'contacto'}
-  //
+{ path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InfoComponent } from './componentes/info/info.component';
 import { AboutComponent } from './componentes/about/about.component';
 import { ContactComponent } from './componentes/contact/contact.component';
-import { DetalleEmpleadoComponent } from './componentes/detalle-empleado/detalle-empleado.component';
-import { ProyectosComponent } from './componentes/empleados/proyectos/proyectos.component';
-import { CurriculumComponent } from './componentes/empleados/curriculum/curriculum.component';
-import { ExperienciaComponent } from './componentes/empleados/experiencia/experiencia.component';
 import { HorariosComponent } from './componentes/horarios/horarios.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfoComponent,
     AboutComponent,
     ContactComponent,
-    DetalleEmpleadoComponent,
-    ProyectosComponent,
-    CurriculumComponent,
-    ExperienciaComponent,
-    HorariosComponent
+    HorariosComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
